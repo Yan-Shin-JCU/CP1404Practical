028410d6e29a62a8a0927f3d5472d82bb2149d78
Yan-Shin-JCU <yan.shin@my.jcu.edu.au>
2021-08-02 22:36:39 +0630

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,5 +1,17 @@
+def main():
+    input_pass = get_password()
+    print_asterisk(input_pass)
 
-input_pass = input('Type Password: ')
-while len(input_pass) <= 1:
+
+def print_asterisk(input_pass):
+    print(len(input_pass) * '*')
+
+
+def get_password():
     input_pass = input('Type Password: ')
-print(len(input_pass) * '*')
+    while len(input_pass) <= 1:
+        input_pass = input('Type Password: ')
+    return input_pass
+
+
+main()
