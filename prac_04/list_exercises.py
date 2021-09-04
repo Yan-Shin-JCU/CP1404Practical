dc93dcf5d2b73689f2c926752547c901b7a35af7
Yan-Shin-JCU <yan.shin@my.jcu.edu.au>
2021-09-04 15:20:47 +0630

    Add list_exercises.py

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,33 @@
+def main():
+    num_list = get_num()
+    first_last(num_list)
+    print(num_list)
+    min_max(num_list)
+    average(num_list)
+
+
+def get_num():
+    count = 0
+    num_list = []
+    while count < 5:
+        num_input = int(input('Number: '))
+        num_list.append(num_input)
+        count += 1
+    return num_list
+
+
+def first_last(num_list):
+    print('The first number is {}'.format(num_list[0]))
+    print('The last number is {}'.format(num_list[-1]))
+
+
+def min_max(num_list):
+    print('The smallest number is {}'.format(min(num_list)))
+    print('The largest number is {}'.format(max(num_list)))
+
+
+def average(num_list):
+    print('The average number is {}'.format(sum(num_list)/len(num_list)))
+
+
+main()
